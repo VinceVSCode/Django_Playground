fbc82979965e38d99508a6914f2ff016684ed510
Themis Sarafidis <sarafidis12@gmail.com>
2025-08-15 13:48:25 +0300

    Add firstsite app in the settings. Implement hello_world view with template rendering

--- DjangoPlayground/DjangoPlayground/settings.py
+++ DjangoPlayground/DjangoPlayground/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Custom apps
+    'firstsite',  # Ensure this matches the name of the app folder
 ]
 
 MIDDLEWARE = [
