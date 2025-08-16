172a9d0b63da84728caa7045c93b6365b603e9f7
Themis Sarafidis <sarafidis12@gmail.com>
2025-08-16 19:39:23 +0300

    Update URL routing and register Note model in admin panel

--- DjangoPlayground/firstsite/urls.py
+++ DjangoPlayground/firstsite/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import hello_world
+from .views import hello_world, user_notes
 
 urlpatterns = [
-    path('', hello_world, name='hello_world'), # Maps the root URL to the hello_world view. Basically /firstsite/ will call the hello_world function.
+    path('hello/', hello_world, name='hello_world'), # Maps the root URL to the hello_world view. Basically /firstsite/ will call the hello_world function.
+    path('notes/',user_notes, name = "user_notes"),
 ] 
