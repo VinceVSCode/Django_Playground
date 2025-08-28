541bcefd33ae8f4fe3253de52db07966f3f501c0
Themis Sarafidis <sarafidis12@gmail.com>
2025-08-28 21:18:53 +0300

    Add token authentication to REST framework settings

--- DjangoPlayground/DjangoPlayground/settings.py
+++ DjangoPlayground/DjangoPlayground/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
 
     # Custom apps
     'firstsite',  # Ensure this matches the name of the app folder
-    'rest_framework'
+    'rest_framework',
+    'rest_framework.authtoken',  # Enable token authentication
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
