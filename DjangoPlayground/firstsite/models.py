36eff15e7c4ad1b111cd27faae0a2911b0c2ff55
Themis Sarafidis <sarafidis12@gmail.com>
2025-08-29 19:45:39 +0300

    Add Tag model and register it in the admin interface.

--- DjangoPlayground/firstsite/models.py
+++ DjangoPlayground/firstsite/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+# Note model
 class Note(models.Model):
     title = models.CharField(max_length = 200)
     content =  models.TextField()
@@ -10,3 +11,12 @@ class Note(models.Model):
     
     def __str__(self):
         return self.title
+
+
+# Tag model
+class Tag(models.Model):
+    name = models.CharField(max_length=16)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
