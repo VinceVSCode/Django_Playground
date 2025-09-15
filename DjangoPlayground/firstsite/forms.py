111138b007bf5bae5de420513cf662394047d29c
Themis Sarafidis <sarafidis12@gmail.com>
2025-09-15 21:10:14 +0300

    - Change forms to fit the new needs.

--- DjangoPlayground/firstsite/forms.py
+++ DjangoPlayground/firstsite/forms.py
@@ -1,7 +1,20 @@
 from django import forms
-from .models import Note
+from .models import Note, Tag
 
 class NoteForm(forms.ModelForm):
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.none(),  # Initial empty queryset
+        required=False,
+        widget=forms.CheckboxSelectMultiple
+    )
     class Meta:
         model = Note
-        fields = ['title', 'content']
+        fields = ["title", "content",  "is_pinned", "is_archived","tags"]
+    def __init__(self, *args, **kwargs):
+        # Extract the user from kwargs
+        user = kwargs.pop('user', None)
+        # Call the parent constructor
+        super().__init__(*args, **kwargs)
+        # Dynamically set the queryset for tags based on the user
+        if user is not None:
+            self.fields['tags'].queryset = Tag.objects.filter(owner=user).order_by('name')
