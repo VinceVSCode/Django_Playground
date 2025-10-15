8d0326516f240df80055f6c238909e088ceb39a3
Themis Sarafidis <sarafidis12@gmail.com>
2025-10-15 22:39:18 +0300

    -Add base, development, and production settings for Django project.

---
+++ DjangoPlayground/settings_prod.py
@@ -0,0 +1,9 @@
+from .settings_base import *
+
+DEBUG = False
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+# Set ALLOWED_HOSTS via env in production, e.g. DJANGO_ALLOWED_HOSTS=yourdomain.com
