a5bdeca9ae0ea3ca13421014e695d88f5f930e82
Themis Sarafidis <sarafidis12@gmail.com>
2025-10-20 20:18:10 +0300

    -Add basic security settings for production in settings_dev.py

--- DjangoPlayground/settings_dev.py
+++ DjangoPlayground/settings_dev.py
@@ -1,2 +1,9 @@
 from .settings_base import *
 DEBUG = True
+
+# Basic security hardening for prod
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
