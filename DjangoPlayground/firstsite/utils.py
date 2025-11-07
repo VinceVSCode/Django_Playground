737461dde3db5a199b9b7041f9c6bdc9f02ef1b6
Themis Sarafidis <sarafidis12@gmail.com>
2025-11-07 23:23:49 +0200

    - Created log_note_event function in utils.py to centralize event logging

--- DjangoPlayground/firstsite/utils.py
+++ DjangoPlayground/firstsite/utils.py
@@ -1,6 +1,6 @@
 from typing import Any
 from django.contrib.auth.models import AnonymousUser
-
+from .models import NoteEvent 
 def attach_actor(instance: Any, user: Any) -> None:
     """
     Attaches the user as the actor to the given instance for auditing purposes.
@@ -8,4 +8,16 @@ def attach_actor(instance: Any, user: Any) -> None:
     """
     if user and not isinstance(user, AnonymousUser):
         setattr(instance, '_actor', user)
+
+def log_note_event(user: Any, note: Any, action: str) -> NoteEvent | None:
+    """
+    Central function that creates analytics/audit events.
+    - user: the acting user (ignored if None/Anonymous)
+    - note: the related Note instance or None (for deletes)
+    - action: one of NoteEvent.ACTION_*
+    """
+
+    if user and not isinstance(user, AnonymousUser):
+        return NoteEvent.objects.create(user=user,note=note,action=action)
     
+    return None
